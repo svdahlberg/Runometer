adfdcdcedb101dee322c24b7c3223f343b4ecad1
Svante <svante.dahlberg@gmail.com>
2018-02-11 21:28:07 +0100

    table view cell

--- Runometer/ViewController/Past runs/PastRunTableViewCell.swift
+++ Runometer/ViewController/Past runs/PastRunTableViewCell.swift
@@ -10,11 +10,23 @@ import UIKit
 
 class PastRunTableViewCell: UITableViewCell {
     
+    @IBOutlet private weak var distanceLabel: UILabel!
+    @IBOutlet private weak var timeLabel: UILabel!
+    @IBOutlet private weak var dateLabel: UILabel!
+    
     var run: Run? {
         didSet {
             guard let run = run,
-                let formattedDistance = DistanceFormatter.format(distance: run.distance) else { return }
-            textLabel?.text = formattedDistance
+                let formattedDistance = DistanceFormatter.format(distance: run.distance),
+                let formattedTime = TimeFormatter.format(time: Seconds(run.duration)),
+                let date = run.timestamp
+                else { return }
+            
+            distanceLabel?.text = "\(formattedDistance) \(AppConfiguration().distanceUnit.symbol)"
+            timeLabel.text = "\(formattedTime)"
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateStyle = .short
+            dateLabel.text = dateFormatter.string(from: date)
         }
     }
 }
