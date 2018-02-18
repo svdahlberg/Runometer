de05149b4a2afcac1d76877eed91f06b03d3e62a
Svante <svante.dahlberg@gmail.com>
2018-02-18 12:24:37 +0100

    group past runs by month in table view

--- Runometer/ViewController/Past runs/PastRunsViewController.swift
+++ Runometer/ViewController/Past runs/PastRunsViewController.swift
@@ -8,13 +8,42 @@
 
 import UIKit
 
+struct RunSection {
+    let title: String
+    let runs: [Run]
+}
+
 class PastRunsViewControlller: UIViewController {
+    
     @IBOutlet private weak var tableView: UITableView!
     
     private var runs: [Run]? {
         didSet {
             tableView.reloadData()
         }
+    }
+
+    private lazy var titleDateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM yyyy"
+        return dateFormatter
+    }()
+    
+    private var tableViewSections: [RunSection]? {
+        guard let runs = runs else { return nil }
+        return Dictionary(grouping: runs) {
+            titleDateFormatter.string(from: $0.timestamp ?? Date())
+        }.map {
+            RunSection(title: $0, runs: $1)
+        }
+    }
+
+    private func tableViewSection(for section: Int) -> RunSection? {
+        return tableViewSections?[section]
+    }
+    
+    private func run(for indexPath: IndexPath) -> Run? {
+        return tableViewSection(for: indexPath.section)?.runs[indexPath.row]
     }
     
     override func viewDidLoad() {
@@ -29,25 +58,11 @@ class PastRunsViewControlller: UIViewController {
     }
 }
 
-extension PastRunsViewControlller: UITableViewDelegate, UITableViewDataSource {
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return runs?.count ?? 0
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "PastRunTableViewCellIdentifier", for: indexPath) as! PastRunTableViewCell
-        cell.run = runs?[indexPath.row]
-        return cell
-    }
-    
+extension PastRunsViewControlller: UITableViewDelegate {
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         switch editingStyle {
         case .delete:
-            guard let run = runs?[indexPath.row] else { return }
+            guard let run = run(for: indexPath) else { return }
             RunService.delete(run)
             runs = RunService().savedRuns()
         default:
@@ -56,7 +71,27 @@ extension PastRunsViewControlller: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let run = runs?[indexPath.row]
+        guard let run = run(for: indexPath) else { return }
         performSegue(withIdentifier: "RunDetailsSegueIdentifier", sender: run)
+    }
+}
+
+extension PastRunsViewControlller: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return tableViewSections?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableViewSection(for: section)?.runs.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PastRunTableViewCellIdentifier", for: indexPath) as! PastRunTableViewCell
+        cell.run = run(for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return tableViewSection(for: section)?.title
     }
 }
