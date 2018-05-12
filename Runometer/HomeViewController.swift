ca566ef2f1cbe7728871d9fba5f2a7cd5e16e63d
Svante <svante.dahlberg@gmail.com>
2018-05-12 11:36:56 +0200

    UI fixes

--- Runometer/HomeViewController.swift
+++ Runometer/HomeViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    @IBOutlet private weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Runometer"
+        startButton.backgroundColor = Colors.green
     }
     
 }
