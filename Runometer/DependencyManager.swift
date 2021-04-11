8f86e1e089d50f0c606df68c2676546a75362925
Svante Dahlberg <svante.dahlberg@bontouch.com>
2021-04-11 09:19:50 -0700

    Add dependency containter as Swift package

--- Runometer/DependencyManager.swift
+++ Runometer/DependencyManager.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import DependencyContainer
 
 class DependencyManager {
 
--- RunometerTests/RunRepositoryTests.swift
+++ RunometerTests/RunRepositoryTests.swift
@@ -8,6 +8,7 @@
 
 import XCTest
 @testable import Runometer
+import DependencyContainer
 
 class RunRepositoryTests: XCTestCase {
 
